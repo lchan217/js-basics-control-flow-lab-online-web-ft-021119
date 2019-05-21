4bcb02c3c1d2e172eeee1329d24f01413bc103fe
Lizzie <lchan217@gmail.com>
2019-05-21 15:43:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num){
+  let result;
+  if (num <= 400) {
+    result = 'This one is on me!';
+  } else if (num > 2000 && num < 2500) {
+      result = 'I will gladly take your thirty bucks.'
+  } else if (num > 2500) {
+        result = 'No can do.'}
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result;
+  city === 'NYC' ? (result='Ok, sounds good.') : (result='No go.');
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result;
+  switch(tip){
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      result = 'Thank you.';
+      break;
+    default:
+      result = 'Bye.';
+      break;
+    }
+    return result
 }
